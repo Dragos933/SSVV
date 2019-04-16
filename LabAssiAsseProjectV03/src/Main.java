a210b604ccb3adaf190b08a1847643eee28e370e
Dragos933 <dragoscornean@yahoo.com>
2019-04-16 20:57:38 +0300

    First commit

---
+++ LabAssiAsseProjectV03/src/Main.java
@@ -0,0 +1,33 @@
+import Exceptions.*;
+import Repository.TxtFileRepository.StudentFileRepo;
+import Repository.XMLFileRepository.NotaXMLRepo;
+import Repository.XMLFileRepository.StudentXMLRepo;
+import Repository.XMLFileRepository.TemaLabXMLRepo;
+import Service.TxtFileService.StudentService;
+import Service.XMLFileService.NotaXMLService;
+import Service.XMLFileService.StudentXMLService;
+import Service.XMLFileService.TemaLabXMLService;
+import Validator.*;
+import UI.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ValidatorException {
+        //System.out.println("Hello World!");
+        StudentValidator vs=new StudentValidator();
+        TemaLabValidator vt=new TemaLabValidator();
+        NotaValidator vn=new NotaValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        //StudentFileRepo strepo=new StudentFileRepo("studenti.txt",vs);
+        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+
+        //StudentService sttt = new StudentService(strepo);;
+
+        StudentXMLService stsrv=new StudentXMLService(strepo);
+        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+        ui ui=new ui(stsrv,tmsrv,ntsrv);
+        ui.run();
+    }
+}
